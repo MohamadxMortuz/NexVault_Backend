ee2b02e35be5bd3dfc496ca958f848d48ce2fa85
MohamadxMortuz <muhammad.meraj7986@gmail.com>
2026-05-14 03:31:35 +0530

    Add orphaned GridFS chunk cleanup on server startup

--- config/db.js
+++ config/db.js
@@ -8,6 +8,17 @@ const connectDB = async () => {
     console.log('MongoDB connected successfully');
     bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, { bucketName: 'uploads' });
     console.log('GridFS bucket ready');
+
+    // Cleanup orphaned GridFS files whose File doc was already deleted
+    const File = require('../models/File');
+    const gridfsFiles = await bucket.find({}).toArray();
+    for (const gf of gridfsFiles) {
+      const exists = await File.findOne({ gridfsId: gf._id });
+      if (!exists) {
+        await bucket.delete(gf._id);
+        console.log(`Cleaned up orphaned GridFS file: ${gf._id}`);
+      }
+    }
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
